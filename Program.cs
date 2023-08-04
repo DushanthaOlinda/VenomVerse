bb09bf06a69ab7b50dea154d4b27223e83e2a5af
Dushantha Olinda <2020cs185@stu.ucsc.cmb.ac.lk>
2023-08-04 10:40:40 +0530

    hosted successfully

--- Program.cs
+++ Program.cs
@@ -42,11 +42,11 @@ builder.Services.AddScoped<TokenService, TokenService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
