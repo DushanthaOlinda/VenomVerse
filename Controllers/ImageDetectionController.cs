bb65bf32e4b3d1d8bb4af5a0fc034255d7316d0d
KimuthuKisal <kimuthukisal23@gmail.com>
2023-09-30 01:06:08 +0530

    ImageScan Dto

--- Controllers/ImageDetectionController.cs
+++ Controllers/ImageDetectionController.cs
@@ -19,34 +19,37 @@ public class ImageDetectionController : ControllerBase
 
     // save scanned image, prediction, accuracy
     [HttpPost("ScanImage")]
-    public async Task<ActionResult<long>> ScanImage(ScannedImageDto scannedImageDto)
+    public async Task<ActionResult> ScanImage(ScannedImageDto scannedImageDto)
     {
+        // return Ok("function inside");
         if (_context.ScannedImage == null)
         {
             return NoContent();
         }
 
-        if (scannedImageDto.UploadedUserId == null)
-        {
-            return NotFound("User not found");
-        }
-
-        var image = new ScannedImage()      // CHECK THIS
-        {
-            ScannedImageId = new Random().NextInt64(),
-            UploadedUserId = scannedImageDto.UploadedUserId,
-            ScannedImageMedia = scannedImageDto.ScannedImageMedia,
-            DateTime = scannedImageDto.DateTime,
-            PredictedSerpentType = scannedImageDto.PredictedSerpentType,
-            Accuracy = scannedImageDto.Accuracy,
-            ActualSerpentType = scannedImageDto.ActualSerpentType,
-            PredictionSuccess = scannedImageDto.PredictionSuccess,
-
-        };
+        var image = ScannedImage.ScannedImageDtoToScannedImage(scannedImageDto);
         _context.ScannedImage.Add(image);
 
         await _context.SaveChangesAsync();
-        return image.ScannedImageId;
+        return CreatedAtAction("GetCommunityPost", new { id = scannedImageDto.ScannedImageId }, scannedImageDto);
+
+        // if (scannedImageDto.UploadedUserId == null)
+        // {
+        //     return NotFound("User not found");
+        // }
+
+        // var image = new ScannedImage()      // CHECK THIS
+        // {
+        //     ScannedImageId = new Random().NextInt64(),
+        //     UploadedUserId = scannedImageDto.UploadedUserId,
+        //     ScannedImageMedia = scannedImageDto.ScannedImageMedia,
+        //     DateTime = scannedImageDto.DateTime,
+        //     PredictedSerpentType = scannedImageDto.PredictedSerpentType,
+        //     Accuracy = scannedImageDto.Accuracy,
+        //     // ActualSerpentType = scannedImageDto.ActualSerpentType,
+        //     // PredictionSuccess = scannedImageDto.PredictionSuccess,
+
+        // };
         // return CreatedAtAction("GetCommunityPost", new { id = communityPost.PostId }, communityPost);
     }
 
